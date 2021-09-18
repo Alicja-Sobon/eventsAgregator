1df8ad38c53ad8731262d26280bf4571e2d350f4
Alicja-Sobon <alicja.sobon19@gmail.com>
2021-09-18 10:23:44 +0200

    initial commit

---
+++ src/main/java/pl/sda/eventsagregator/EventsAgregatorApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.eventsagregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsAgregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsAgregatorApplication.class, args);
+    }
+
+}
